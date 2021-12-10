7cbe6431d22137093af3841bc4581e97f9f72b7c
jackson_0427 <t0427t19@gmail.com>
2021-12-10 10:43:08 +0900

    Add scanf.printf.cpp

---
+++ scanf.printf.cpp
@@ -0,0 +1,18 @@
+
+/*
+C言語学習記録_jackson_0427
+入手出力形式
+c_lang...scanf, printf //cin, coutと比較して高速
+c++...cin, cout
+*/
+#include<stdio.h>
+
+int main(void)
+{
+    int x, y;
+    scanf("%d %d", &x, &y); //整数値x, yに入力.
+
+    printf("%d", x + y); //整数値xとyを加えた結果を表示.
+    
+    return 0;
+}
