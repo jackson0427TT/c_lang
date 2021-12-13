a9447478f6fb60ef9f3656abddd4d5ea225b072f
jackson_0427 <t0427t19@gmail.com>
2021-12-13 20:57:27 +0900

    [Add]double.c

---
+++ double.c
@@ -0,0 +1,20 @@
+
+/*
+C言語学習記録_jackson_0427
+double型の使用.
+ABC231-Aを利用して学習.
+*/
+#include<stdio.h>
+
+int main(void) {
+    //入力.
+    double D;
+    scanf("%lf", &D);
+
+    //計算.intとdoubleではint→doubleの型変換が起こる.
+    double ans = D / 100;
+
+    //出力.精度指定は.(数字).
+    printf("%.10f", ans);
+    return 0;
+}
